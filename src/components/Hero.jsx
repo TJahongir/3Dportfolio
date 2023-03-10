5e8e9f3488a3fe543ada93de71658659b3dd12df
TJahongir <jakhongirt@gmail.com>
2023-03-10 17:17:56 +0500

    finished hero 3d

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../style";
-import { ComputersCanvas } from "./canvas";
+import Computers, { ComputersCanvas } from "./canvas/Computers";
 
 const Hero = () => {
 	return (
